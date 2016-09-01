ac9de919ea2e4e34ef9ca8d9f0e57e2aa47e8a9c
Egor Biriukov <birykovegor@gmail.com>
2016-09-01 21:31:44 +0300

    Events module with interface.

---
+++ 13/events.erl
@@ -0,0 +1,40 @@
+-module(events).
+-author("egor").
+
+%% API
+-compile(export_all).
+-record(state, {server, name="", to_go=0}).
+
+start(EventName, Delay) ->
+  spawn(?MODULE, init, [self(), EventName, Delay]).
+
+init(Server, Name, Delay) ->
+  NewDelay = normalize(Delay),
+  io:format("Delay: ~p~n", [NewDelay]),
+  loop(#state{server = Server, name = Name, to_go = NewDelay}).
+
+cancel(Pid) ->
+  Ref = erlang:monitor(process, Pid),
+  Pid ! {self(), Ref, cancel},
+  receive
+    {Ref, ok} ->
+      erlang:demonitor(Ref, [flush]),
+       ok;
+    {'DOWN', Ref, process, Pid, _Reason} ->
+      ok
+  end.
+
+loop(S = #state{server=Server, to_go=[T|Next]}) ->
+  receive
+    {Server, Ref, cancel} ->
+      Server ! {Ref, ok}
+  after T * 1000 ->
+    case Next of
+      [] -> Server ! {done, S#state.name};
+      _ -> loop(S=#state{to_go = Next})
+    end
+  end.
+
+normalize(N) ->
+  Limit = 49 * 24 * 60 * 60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
