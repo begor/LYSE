ef2afa7c3f2e0e3185706abc3831b8e31e7aebd8
Egor Biriukov <birykovegor@gmail.com>
2016-09-04 14:29:57 +0300

    Generic server API.

--- 14/my_server.erl
+++ 14/my_server.erl
@@ -1,14 +1,14 @@
 %%% Simple generic server. Abstracts sync and async messages, main loop, termination, etc.
 -module(my_server).
--export([call/2]).
+-export([call/2, cast/2, loop/2, reply/2]).
 
 %% @doc Call a synchronous message Msg on process Pid.
 call(Pid, Msg) ->
   Ref = erlang:monitor(process, Pid),
-  Pid ! {self(), Ref, Msg},
+  Pid ! {sync, self(), Ref, Msg},
   receive
     {Ref, Reply} ->
-      erlang:demonitor(Ref),
+      erlang:demonitor(Ref, [flush]),
       Reply;
     {'DOWN', Ref, process, Pid, Reason} ->
       erlang:error(Reason)
@@ -16,4 +16,23 @@ call(Pid, Msg) ->
     erlang:error(timeout)
   end.
 
+%% @doc Cast an asynchronous message Msg on process Pid.
+cast(Pid, Msg) ->
+  Pid ! {async, Msg},
+  ok.
 
+%% @doc Abstracts main loop of a process.
+loop(Module, State) ->
+  receive
+    {sync, Pid, Ref, Msg} ->
+      % Clients don't need to know about Refs, so we put {Pid, Ref} in a tuple From.
+      NewState = Module:handle_call(Msg, {Pid, Ref}, State),
+      loop(Module, NewState);
+    {async, Msg} ->
+      NewState = Module:handle_cast(Msg, State),
+      loop(Module, NewState)
+  end.
+
+%% @doc Abstracts sending a reply.
+reply({Pid, Ref}, Reply) ->
+  Pid ! {Ref, Reply}.
