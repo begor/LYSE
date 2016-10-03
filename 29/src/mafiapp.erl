962d8c4addbe48ef3275f181f7ea1a34470160fa
Egor Biriukov <birykovegor@gmail.com>
2016-10-03 17:58:56 +0300

    Query List Comprehensions

--- 29/src/mafiapp.erl
+++ 29/src/mafiapp.erl
@@ -1,6 +1,7 @@
 -module(mafiapp).
 -behaviour(application).
 -include_lib("stdlib/include/ms_transform.hrl").
+-include_lib("stdlib/include/qlc.hrl"). % Query List Comprehensions
 
 -export([install/1, start/2, stop/1, add_friend/4, add_service/4, friend_by_name/1, friend_by_expertise/1, debts/1, add_enemy/2, find_enemy/1, enemy_killed/1]).
 
@@ -60,15 +61,16 @@ find_services(Name) ->
   ),
   mnesia:select(mafiapp_services, Match).
 
+% Notice using QLCs.
 friend_by_expertise(Expertise) ->
-  Pattern = #mafiapp_friends{_ = '_',
-    expertise = Expertise},
   F = fun() ->
-    Res = mnesia:match_object(Pattern),
-    [{Name, C, I, Expertise, find_services(Name)} ||
-      #mafiapp_friends{name = Name,
-        contact = C,
-        info = I} <- Res]
+    qlc:eval(qlc:q(
+      [{Name, C, I, E, find_services(Name)} ||
+        #mafiapp_friends{name = Name,
+          contact = C,
+          info = I,
+          expertise = E} <- mnesia:table(mafiapp_friends),
+        E =:= Expertise]))
       end,
   mnesia:activity(transaction, F).
 
@@ -87,29 +89,32 @@ add_service(From, To, Date, Description) ->
   mnesia:activity(transaction, F).
 
 debts(Name) ->
-  Match = ets:fun2ms(
-    fun(#mafiapp_services{from = From, to = To}) when From =:= Name ->
-      {To, -1};
-      (#mafiapp_services{from = From, to = To}) when To =:= Name ->
-        {From, 1}
-    end),
-  F = fun() -> mnesia:select(mafiapp_services, Match) end,
-  Dict = lists:foldl(fun({Person, N}, Dict) ->
-    dict:update(Person, fun(X) -> X + N end, N, Dict)
-                     end,
-    dict:new(),
-    mnesia:activity(transaction, F)),
-  lists:sort([{V, K} || {K, V} <- dict:to_list(Dict)]).
+  F = fun() ->
+    QH = qlc:q(
+      [if Name =:= To -> {From, 1};
+         Name =:= From -> {To, -1}
+       end || #mafiapp_services{from = From, to = To} <-
+        mnesia:table(mafiapp_services),
+        Name =:= To orelse Name =:= From]),
+    qlc:fold(
+      fun({Person, N}, Dict) ->
+        dict:update(Person, fun(X) -> X + N end, N, Dict)
+      end,
+      dict:new(),
+      QH
+    )
+      end,
+  lists:sort([{V, K} || {K, V} <- dict:to_list(mnesia:activity(transaction, F))]).
 
 add_enemy(Name, Info) ->
-  F = fun() -> mnesia:write(#mafiapp_enemies{name=Name, info=Info}) end,
+  F = fun() -> mnesia:write(#mafiapp_enemies{name = Name, info = Info}) end,
   mnesia:activity(transaction, F).
 
 find_enemy(Name) ->
   F = fun() -> mnesia:read({mafiapp_enemies, Name}) end,
   case mnesia:activity(transaction, F) of
     [] -> undefined;
-    [#mafiapp_enemies{name=N, info=I}] -> {N,I}
+    [#mafiapp_enemies{name = N, info = I}] -> {N, I}
   end.
 
 enemy_killed(Name) ->
